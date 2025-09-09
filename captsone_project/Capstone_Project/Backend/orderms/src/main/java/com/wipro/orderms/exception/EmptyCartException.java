134c97a8827f67038ee29878e6b65d2b65278784
premkumar085 <naniprem085@gmail.com>
2025-09-09 08:48:43 +0530

    added

--- captsone_project/Capstone_Project/Backend/orderms/src/main/java/com/wipro/orderms/exception/EmptyCartException.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.orderms.exception;
-
-public class EmptyCartException extends RuntimeException {
-    public EmptyCartException(String message) {
-        super(message);
-    }
-}
--- captsone_project/Capstone_Project/Backend/orderms/src/main/java/com/wipro/orderms/exception/InvalidOrderStatusException.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.orderms.exception;
-
-public class InvalidOrderStatusException extends RuntimeException {
-    public InvalidOrderStatusException(String message) {
-        super(message);
-    }
-}
